d1359a39b701280ce63ec862e753a5142e2e51c8
SonaliDasgupta <sonalidasgupta95.2011@gmail.com>
2018-08-03 14:56:43 +0530

    Encapsulated MongoDB ops in Node Module

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require("assert");
+const dbOp= require('./operations');
 
 const url= "mongodb://localhost:27017/conFusion";
 const dbName="conFusion";
@@ -10,21 +11,26 @@ MongoClient.connect(url, (err, client)=>{
 	assert.equal(err, null);
 	console.log("Connected correctly to server");
 	const db=client.db(dbName);
-	const collection = db.collection("dishes");
-	collection.insertOne({"name":"Uthappizza","description":"test"}, 
-		(err, result)=> {
-			assert.equal(err, null);
-			console.log("After insert:\n");
-			console.log(result.ops);
+	dbOp.insertDocument(db, {name: "Vadonut", description: "test"}, "dishes", (result)=> {
+		console.log("Inserted Document: \n", result.ops);
 
-			collection.find({}).toArray((err, docs)=>{
-				assert.equal(err,null);
-				console.log("Found: \n");
-				console.log(docs);
-				db.dropCollection("dishes", (err, result)=>{
-					assert.equal(err, null);
-					client.close();
-				});
+		dbOp.findDocuments(db, "dishes", (docs)=>{
+			console.log("Found documents:\n ", docs);
+
+			dbOp.updateDocument(db, {name: "Vadonut"}, {"description": "Updated test"}, "dishes", (result)=>{
+				console.log("Updated Document \n ", result.result);
+
+				dbOp.findDocuments(db, "dishes", (docs)=>{
+			console.log("Found documents:\n ", docs);
+
+			db.dropCollection("dishes",(result)=>{
+				console.log("Dropped collection: ",result);
+				client.close();
 			});
 		});
+
+
+			});
+		})
+	});
 });
