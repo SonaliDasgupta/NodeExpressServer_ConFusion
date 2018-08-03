7c2b21174c7ad55f1354996917dc3e034bc8cdba
SonaliDasgupta <sonalidasgupta95.2011@gmail.com>
2018-08-03 20:39:19 +0530

    Schema using Mongoose

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -2,29 +2,46 @@ const mongoose = require("mongoose");
 
 const Dishes = require("./models/dishes");
 
+
 const url = "mongodb://localhost:27017/conFusion";
-const connect = mongoose.connect(url, {
-	useMongoClient: true
-});
+const connect = mongoose.connect(url);
 
 connect.then((db) => {
 	//const db=client.db("conFusion");
 	console.log("Connected to database");
 
-	Dishes.create({
+	return Dishes.create({
 		name: "abc5",
 		description: "def"
-	}).then((dish)=>{
+	})
+.then((dish)=>{
 		console.log(dish);
-		return Dishes.find({}).exec();
-	}).then((dishes)=>{
-		console.log(dishes);
-
+		
+		return Dishes.findByIdAndUpdate(dish._id,{$set: {description: 'abc'}},
+				{ 
+					new: true
+				}
+		).exec();	
+		
+		})
+	.then((dish)=>{
+		console.log(dish);
+		dish.comments.push({
+			rating: 5,
+			comment: "Good",
+			author: "me"
+		});
+		return dish.save();
+	}).then((dish)=> {
+		console.log(dish);
 		return mongoose.connection.db.dropCollection('dishes');
+	
 	}).then(()=>{
 		return mongoose.connection.close();
 	}).catch((err)=>{
 		console.log(err);
 	});
-
 });
+
+
+
