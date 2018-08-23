1709c6f4b0ac727ca19c28fa5a8318da3ac28fda
SonaliDasgupta <sonalidasgupta95.2011@gmail.com>
2018-08-23 21:48:42 +0530

    Add Request Query feature

--- routes/leader-router.js
+++ routes/leader-router.js
@@ -6,7 +6,7 @@ const cors= require('./cors');
 const leaderRouter = express.Router();
 leaderRouter.use(bodyParser.json());
 leaderRouter.route("/").options(cors.corsWithOptions, (req, res)=>{ res.sendStatus(200); }).get(cors.cors, (req, res, next)=>{
-	Leaders.find({})
+	Leaders.find(req.query)
 	.then((leaders)=>{
 		res.statusCode= 200;
 		res.setHeader('Content-Type','application/json');
